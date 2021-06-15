2d958436d8af601d96ff977ecf71b19827aa8272
vicotr <victor@within.co.kr>
2021-06-15 22:49:01 +0900

    bean 설정 정보에 초기화 메서드, 종료 메서드 :  지정 권장되지 않는 방법

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,16 +24,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("disconnect");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
-        disconnect();
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 메세지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
